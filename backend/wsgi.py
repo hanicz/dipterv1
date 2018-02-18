ec55840186efbeb2a68691bab0e810cfe04e6f44
hanicz <hanicz20@gmail.com>
2018-02-18 14:43:01 +0100

    wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,10 @@
+import os
+
 from main import app as application
+from models import delete_job
 
 if __name__ == "__main__":
+    delete_job()
+
+    application.secret_key = os.getenv('SECRET_KEY')
     application.run()
